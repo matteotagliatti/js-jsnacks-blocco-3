7f0b358632ca00136a807d927006887ccdb76cd3
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-02 16:47:36 +0100

    snack 3 BONUS

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,17 +1,28 @@
 const output = document.getElementById("output");
 
-let number = parseInt(prompt("inserisci un numero"));
+let number = parseInt(prompt("inserisci un numero N"));
+const m = parseInt(prompt("inserisci un secondo numero M"));
+const l = parseInt(prompt("inserisci un terzo numero L"));
+
 if (number > 100) number = 100;
 
 const array = [];
 
 for (let i = 0; i < number; i++) {
   array[i] = [];
+  let sum = 0;
+  let outputSum1 = "false";
+  let outputSum2 = "false";
 
-  while (array[i].length < 10) {
+  while (array[i].length < m) {
     const arrayNumber = Math.floor(Math.random() * 4528 + 4);
     array[i].push(arrayNumber);
+    sum += arrayNumber;
+    if (sum > l) outputSum1 = sum;
+    if (sum < l) outputSum2 = sum;
   }
 
-  output.innerHTML += `<li>${array[i].join(", ")}</li>`;
+  output.innerHTML += `<li>Array${[i]}: <span>${array[i].join(
+    ", "
+  )}</span> - Sum > L: <span>${outputSum1}</span> - Sum < L: <span>${outputSum2}</span></li>`;
 }
